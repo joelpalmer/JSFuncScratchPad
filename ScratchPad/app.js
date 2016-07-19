4b591cb448fb9755e65bfe19fa8f8cc5b0da99aa
joelpalmer <joel.palmer@demand-energy.com>
2016-07-19 11:41:51 -0500

    factorial tail recursive with default arg

--- ScratchPad/app.js
+++ ScratchPad/app.js
@@ -1,7 +1,8 @@
-const factorial = (n) =>
-    n == 1
-    ? n
-    : n * factorial(n-1);
+const factorial = (n, work = 1) =>
+    n === 1
+    ? work
+    : factorial(n - 1, n * work);
 
 console.log(factorial(1));
+
 console.log(factorial(5));
