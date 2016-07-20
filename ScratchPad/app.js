972e031654e9f79fa01897646cfe48fb3d35865d
joelpalmer <joel.palmer@demand-energy.com>
2016-07-20 11:05:54 -0500

    abs func

--- ScratchPad/app.js
+++ ScratchPad/app.js
@@ -1,8 +1,8 @@
-const factorial = (n, work = 1) =>
-    n === 1
-    ? work
-    : factorial(n - 1, n * work);
+const Mathematics = {
+    abs: (a) => 
+    a < 0 
+    ? -a 
+    : a
+};
 
-console.log(factorial(1));
-
-console.log(factorial(5));
+console.log(Mathematics.abs(5));
