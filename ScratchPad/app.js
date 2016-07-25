b74fc0918f30a918f4e6d5ff83765853606b6fd3
joelpalmer <joel.palmer@demand-energy.com>
2016-07-25 13:42:26 -0500

    splat stuff

--- ScratchPad/app.js
+++ ScratchPad/app.js
@@ -1,8 +1,31 @@
-const Mathematics = {
-    abs: (a) => 
-    a < 0 
-    ? -a 
-    : a
-};
+//splat
+function splat(fun){
+    return function(array){
+        return fun.apply(null, array);
+    };
+}
 
-console.log(Mathematics.abs(5));
+var addArrayElements = splat(function(x, y) {
+    return x + y;
+});
+
+console.log(`splat: ${addArrayElements([1,2])}`);
+//todo: UT for splat
+//todo: move splat stuff to seperate file
+
+//unsplat
+
+function unsplat(fun){
+    return function(){
+        return fun.call(null, _.toArray(arguments));
+    };
+}
+
+var joinElements = unsplat(function(array){
+    return array.join(' ')
+});
+
+console.log(`unsplat: ${joinElements(1,2)}`);
+
+var whatisthis = joinElements(2, 3);
+console.log(typeof whatisthis);
