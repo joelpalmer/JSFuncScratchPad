6080ec6114268489201eee939822ad97ccfd061a
joelpalmer <joelpalmer@gmail.com>
2017-03-31 13:53:49 -0500

    updates to cwr

--- composeWithReduce.js
+++ composeWithReduce.js
@@ -1,1 +1,12 @@
 const compose = (...funcs) => x => funcs.reduceRight((v, f) => f(v), x);
+const pipe = (...funcs) => x => funcs.reduce((v, f) => f(v), x);
+
+const add1 = n => n + 1;
+const double = n => n * 2;
+
+const add1ThenDouble = compose(
+    double,
+    add1
+);
+
+console.log(add1ThenDouble(2));
